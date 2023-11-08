ef9a11c324bec09430168c69dad12e79e4ee7733
takano.s <takano.s@example.com>
2023-11-08 15:20:39 +0900

    4

--- src/App.js
+++ src/App.js
@@ -17,6 +17,42 @@ const msalConfig = {
 
 const myMSALObj = new msal.PublicClientApplication(msalConfig);
 
+function getAccount() {
+  // アカウント情報をキャッシュから取得
+  const accounts = myMSALObj.getAllAccounts();
+  if (accounts.length > 0) {
+    return accounts[0];
+  } else {
+    return null;
+  }
+}
+
+function signIn() {
+  // ログイン処理（ユーザーがまだログインしていない場合）
+  myMSALObj.loginPopup()
+    .then(loginResponse => {
+      console.log('id_token acquired at: ' + new Date().toString());
+      if (myMSALObj.getAccount()) {
+        console.log('ログイン成功');
+        // 認証情報を取得
+        const account = getAccount();
+        // accountオブジェクトには、ユーザーの名前やIDなどの情報が含まれます
+        console.log(`User's name: ${account.name}`);
+      }
+    }).catch(error => {
+      console.log(error);
+    });
+}
+
+// 認証情報がキャッシュにあるか確認し、あれば取得
+const account = getAccount();
+if (!account) {
+  // アカウントが見つからない場合、サインイン関数を呼び出す
+  signIn();
+} else {
+  // アカウントが存在する場合、必要な操作を行う
+  console.log(`User's name: ${account.name}`);
+}
 
 function App() {
   return (
