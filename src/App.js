b8388af6a94195a814a4a808c4e64a7664571833
takano.s <takano.s@example.com>
2023-11-08 15:13:52 +0900

    2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
 
+import * as msal from '@azure/msal-browser';
+
+const msalConfig = {
+  auth: {
+    clientId: 'YOUR_CLIENT_ID', // Azure ADでのアプリ登録から取得
+    authority: 'https://login.microsoftonline.com/YOUR_TENANT_ID', // テナント情報
+    redirectUri: 'YOUR_REDIRECT_URI' // リダイレクトURI
+  },
+  cache: {
+    cacheLocation: "localStorage", // キャッシュをlocalStorageに設定
+    storeAuthStateInCookie: true // クロスサイトのシナリオで必要な場合
+  }
+};
+
+const myMSALObj = new msal.PublicClientApplication(msalConfig);
+
+
 function App() {
   return (
     <div className="App">
